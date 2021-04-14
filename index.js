f59855a1c55595ef6645201c7ac8f2c32ccce01e
Saloni <salonimehta2127@gmail.com>
2021-04-14 00:53:32 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+
+const main=document.getElementById('main');
+main.remove();
+ const newHeader= document.createElement('h1');
+ document.body.appendChild(newHeader);
+ newHeader.id='victory';
+newHeader.innerHTML="<h1>Saloni is the champion </h1>"
+newHeader.style.backgroundColor='#27647b';
+
+
