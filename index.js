180be8f65bf604a4627a3993f61643d63effd0e2
Saloni <salonimehta2127@gmail.com>
2021-05-10 21:10:21 -0400

    commit tree 2ca643ebeecf933b5919a933948f5a5e21ab479c

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
+//Phase1 attempt
 
 const main=document.getElementById('main');
 main.remove();
- const newHeader= document.createElement('h1');
- document.body.appendChild(newHeader);
- newHeader.id='victory';
-newHeader.innerHTML="<h1>Saloni is the champion </h1>"
-newHeader.style.backgroundColor='#27647b';
+const newHeader=document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id='victory';
+newHeader.innerHTML="Saloni is the champion";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//prework Attempt
+
+
+
+
+
+
+
+// const main=document.getElementById('main');
+// main.remove();
+//  const newHeader= document.createElement('h1');
+//  document.body.appendChild(newHeader);
+//  newHeader.id='victory';
+// newHeader.innerHTML="<h1>Saloni is the champion </h1>"
+// newHeader.style.backgroundColor='#27647b';
 
 
